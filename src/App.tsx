1462c5245c7b26ead8a39e817342f067457accd6
DuckBat <32737124+duckbat@users.noreply.github.com>
2024-01-29 15:17:51 +0200

    first-component first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Welcome from './Welcome'
+import Welcome from './components/Welcome'
+import Home from './components/Home';
 
 const App = () => {
   const person = {name: 'John', age: 25, location: 'Finland'}
   return (
     <> {/* can be div element */}
-      <h1>DuckBat's React App</h1>
+      <Home/>
       <Welcome name={person.name + " you are in " + person.location }/>
       <Welcome name="Sara"/>
       <Welcome name="Ville"/>
